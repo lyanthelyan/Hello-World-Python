10fd4c4942c1aecd3d37d58a95d2ad28e7951c0a
Elyan <emyo.elyan@gmail.com>
2023-05-04 10:23:20 -0300

    Update modulo.py

--- Projeto JSON/modulo.py
+++ Projeto JSON/modulo.py
@@ -1,8 +1,27 @@
+# Criando arquivos com Python + Context Manager with
+# Usamos a função open para abrir
+# um arquivo em Python (ele pode ou não existir)
+# Modos:
+# r (leitura), w (escrita), x (para criação)
+# a (escreve ao final), b (binário)
+# t (modo texto), + (leitura e escrita)
+# Context manager - with (abre e fecha)
+# Métodos úteis
+# write, read (escrever e ler)
+# writelines (escrever várias linhas)
+# seek (move o cursor)
+# readline (ler linha)
+# readlines (ler linhas)
+# Vamos falar mais sobre o módulo os, mas:
+# os.remove ou unlink - apaga o arquivo
+# os.rename - troca o nome ou move o arquivo
+# Vamos falar mais sobre o módulo json, mas:
+# json.dump = Gera um arquivo json
+# json.load
 caminho_arquivo = "C:\\Users\\T-GAMER\\Documents\\Workspace\\Hello World Python\\Projeto JSON\\"
 caminho_arquivo += 'aula116.txt'
 # arquivo = open(caminho_arquivo, 'w')
 # arquivo.write('Oi')
 # arquivo.close()
-
 with open(caminho_arquivo, 'w') as arquivo:
     arquivo.write('Ola Mundo')
